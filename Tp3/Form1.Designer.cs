342f8b8a3191d671055d7449497c06087fefeef6
lucas <lucas@LUCAS>
2021-10-22 01:13:08 -0300

    Agregar archivos de proyecto.

---
+++ Tp3/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿
+namespace Tp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxNombre = new System.Windows.Forms.TextBox();
+            this.textBoxApellido = new System.Windows.Forms.TextBox();
+            this.textBoxFecha = new System.Windows.Forms.TextBox();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblApellido = new System.Windows.Forms.Label();
+            this.lblFecha = new System.Windows.Forms.Label();
+            this.Titulo = new System.Windows.Forms.Label();
+            this.BtInscripcion = new System.Windows.Forms.Button();
+            this.lblInscripcion = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.BtBorrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxNombre
+            // 
+            this.textBoxNombre.Location = new System.Drawing.Point(62, 184);
+            this.textBoxNombre.Name = "textBoxNombre";
+            this.textBoxNombre.Size = new System.Drawing.Size(100, 22);
+            this.textBoxNombre.TabIndex = 0;
+            // 
+            // textBoxApellido
+            // 
+            this.textBoxApellido.Location = new System.Drawing.Point(234, 184);
+            this.textBoxApellido.Name = "textBoxApellido";
+            this.textBoxApellido.Size = new System.Drawing.Size(100, 22);
+            this.textBoxApellido.TabIndex = 1;
+            // 
+            // textBoxFecha
+            // 
+            this.textBoxFecha.Location = new System.Drawing.Point(402, 184);
+            this.textBoxFecha.Name = "textBoxFecha";
+            this.textBoxFecha.Size = new System.Drawing.Size(100, 22);
+            this.textBoxFecha.TabIndex = 2;
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(59, 124);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(58, 17);
+            this.lblNombre.TabIndex = 3;
+            this.lblNombre.Text = "Nombre";
+            // 
+            // lblApellido
+            // 
+            this.lblApellido.AutoSize = true;
+            this.lblApellido.Location = new System.Drawing.Point(231, 124);
+            this.lblApellido.Name = "lblApellido";
+            this.lblApellido.Size = new System.Drawing.Size(58, 17);
+            this.lblApellido.TabIndex = 4;
+            this.lblApellido.Text = "Apellido";
+            // 
+            // lblFecha
+            // 
+            this.lblFecha.AutoSize = true;
+            this.lblFecha.Location = new System.Drawing.Point(399, 124);
+            this.lblFecha.Name = "lblFecha";
+            this.lblFecha.Size = new System.Drawing.Size(127, 17);
+            this.lblFecha.TabIndex = 5;
+            this.lblFecha.Text = "Año de Nacimiento";
+            // 
+            // Titulo
+            // 
+            this.Titulo.AutoSize = true;
+            this.Titulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Titulo.Location = new System.Drawing.Point(342, 28);
+            this.Titulo.Name = "Titulo";
+            this.Titulo.Size = new System.Drawing.Size(134, 29);
+            this.Titulo.TabIndex = 6;
+            this.Titulo.Text = "Inscripcion";
+            // 
+            // BtInscripcion
+            // 
+            this.BtInscripcion.Location = new System.Drawing.Point(588, 183);
+            this.BtInscripcion.Name = "BtInscripcion";
+            this.BtInscripcion.Size = new System.Drawing.Size(75, 23);
+            this.BtInscripcion.TabIndex = 7;
+            this.BtInscripcion.Text = "Inscribir";
+            this.BtInscripcion.UseVisualStyleBackColor = true;
+            this.BtInscripcion.Click += new System.EventHandler(this.BtInscripcion_Click);
+            // 
+            // lblInscripcion
+            // 
+            this.lblInscripcion.AutoSize = true;
+            this.lblInscripcion.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInscripcion.Location = new System.Drawing.Point(71, 273);
+            this.lblInscripcion.Name = "lblInscripcion";
+            this.lblInscripcion.Size = new System.Drawing.Size(0, 29);
+            this.lblInscripcion.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(399, 150);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 17);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "(Ej: 2004)";
+            // 
+            // BtBorrar
+            // 
+            this.BtBorrar.Location = new System.Drawing.Point(588, 231);
+            this.BtBorrar.Name = "BtBorrar";
+            this.BtBorrar.Size = new System.Drawing.Size(75, 23);
+            this.BtBorrar.TabIndex = 10;
+            this.BtBorrar.Text = "Limpiar";
+            this.BtBorrar.UseVisualStyleBackColor = true;
+            this.BtBorrar.Click += new System.EventHandler(this.BtBorrar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BtBorrar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblInscripcion);
+            this.Controls.Add(this.BtInscripcion);
+            this.Controls.Add(this.Titulo);
+            this.Controls.Add(this.lblFecha);
+            this.Controls.Add(this.lblApellido);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.textBoxFecha);
+            this.Controls.Add(this.textBoxApellido);
+            this.Controls.Add(this.textBoxNombre);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxNombre;
+        private System.Windows.Forms.TextBox textBoxApellido;
+        private System.Windows.Forms.TextBox textBoxFecha;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblApellido;
+        private System.Windows.Forms.Label lblFecha;
+        private System.Windows.Forms.Label Titulo;
+        private System.Windows.Forms.Button BtInscripcion;
+        private System.Windows.Forms.Label lblInscripcion;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button BtBorrar;
+    }
+}
+
---
+++ Tp3/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tp3
+{
+    public partial class Form1 : Form
+    {
+        string Nombre;
+        string Apellido;
+        int AñoDeNacimiento;
+
+        public string NombreCompleto()
+        {
+            return Nombre + " " + Apellido;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtInscripcion_Click(object sender, EventArgs e)
+        {
+            Nombre = textBoxNombre.Text;
+            Apellido = textBoxApellido.Text;
+            AñoDeNacimiento = Convert.ToInt32(textBoxFecha.Text);
+
+            if ( 2021 -AñoDeNacimiento >=18)
+            {
+                lblInscripcion.Text = NombreCompleto() + " se inscribio correctamente";
+            }
+            else
+            {
+                lblInscripcion.Text = "Lo sentimos. No cumple con el requisito de edad";
+            }
+            
+        }
+
+        private void BtBorrar_Click(object sender, EventArgs e)
+        {
+            textBoxNombre.Text = "";
+            textBoxApellido.Text = "";
+            textBoxFecha.Text = "";
+            lblInscripcion.Text = "";
+        }
+    }
+}
